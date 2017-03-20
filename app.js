00bbb8c32ebd9e7c0e6ee0277e644116647cea39
smckeon <mckeon.sean@outlook.com>
2017-03-19 23:33:56 -0400

    added window refresh upon game over

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-var correctGuess = false;
-var randomNumber = Math.floor(Math.random() * 8 ) + 1;
-var guess = prompt('I am thinking of a number between 1 and 8. What is it?');
+  var correctGuess = false;
+  var randomNumber = Math.floor(Math.random() * 8 ) + 1;
+  var guess = prompt('I am thinking of a number between 1 and 8. What is it?');
 
-if (parseInt(guess) === randomNumber ) {
-  correctGuess = true;
-} else if (parseInt(guess) < randomNumber) {
-  var guessMore = prompt('Try again. The number is larger than ' + guess);
-    if (parseInt(guessMore) === randomNumber) {
-      correctGuess = true;
+  if (parseInt(guess) === randomNumber ) {
+    correctGuess = true;
+  } else if (parseInt(guess) < randomNumber) {
+    var guessMore = prompt('Try again. The number is larger than ' + guess);
+      if (parseInt(guessMore) === randomNumber) {
+        correctGuess = true;
+    }
+  } else if (parseInt(guess) > randomNumber) {
+    var guessLess = prompt('Try again. The number is smaller than ' + guess);
+      if (parseInt(guessLess) === randomNumber) {
+        correctGuess = true;
+    }
   }
-} else if (parseInt(guess) > randomNumber) {
-  var guessLess = prompt('Try again. The number is smaller than ' + guess);
-    if (parseInt(guessLess) === randomNumber) {
-      correctGuess = true;
+  if ( correctGuess ) {
+      document.write('<p>You guessed the number! As you know, it was the number ' + randomNumber + '.</p>');
+  } else {
+      document.write('<p>Sorry. The correct answer was ' + randomNumber + '.</p>');
   }
-}
-if ( correctGuess ) {
-    document.write('<p>You guessed the number! As you know, it was the number ' + randomNumber + '.</p>');
-} else {
-    document.write('<p>Sorry. The correct answer was ' + randomNumber + '.</p>');
-}
+
+  function refresh() {
+       window.location.reload(true);
+       setTimeout(refresh, 1000);
+   }
+    setTimeout(refresh, 2000);
